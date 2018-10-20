874dcd4f92e831d0a27570974fff166748ef748b
Nico Bellemont <nico.bellemont@gmail.com>
2018-10-20 14:03:25 +0200

    creating poche object

---
+++ poche.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author : Nicolas
+"""
+class Poche():
+    """docstring for Poche."""
+    def __init__(self,contenu):
+        self.contenu = contenu
+        self.phenotype = False
+        self.irradier = False
+        self.code_identification = 0
