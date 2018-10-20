85911c155fbe8d6840233683c180bd0c29380433
Nico Bellemont <nico.bellemont@gmail.com>
2018-10-20 04:19:48 +0200

    modify to csv

--- recuperation.py
+++ recuperation.py
@@ -6,5 +6,5 @@ cle=["ud","sh","gc","ge","rd","rf","rj","rl","rm","rn","rp","pj","pm","qd","qh"]
 with open("181017151004_2119332-Anonyme.AFN.txt", 'r', encoding='utf-8') as file:
     for line in file:
         if str(line[0]+line[1]) in cle:
-            with open("AFN_transpose", 'a') as result:
-                result.write(line[3:])
+            with open("AFN_transpose.csv", 'a') as result:
+                result.write(line)
