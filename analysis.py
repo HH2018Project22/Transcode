6f1b2f5c2ece4082dfe6697ca8a332a069a1fae4
tturlin <theo.turlin.22@gmail.com>
2018-10-20 03:57:46 +0200

    editing afnor file and creating a file, that would be use for compareason with entry before inlog

--- analysis.py
+++ analysis.py
@@ -1,7 +1,10 @@
 # .*. coding: utf-8 .*.
 import csv
+import os
 
 dict_code=dict()
+values=[]
+key=[]
 
 with open("Code_Facturation_Inlog.csv") as facturation:
     dict_temp=dict()
@@ -16,3 +19,27 @@ with open("Code_Produits_Inlog.csv") as products:
     for row in temp:
         dict_temp[row[0]]=row[1]
         dict_code.update(dict_temp)
+
+for i in dict_code.keys():
+    key.append(str(i))
+
+with open("test.csv") as csvfile:
+    temp = csv.reader(csvfile)
+    for row in temp:
+        val=row[1]
+        if val[0]=='0':
+            val=val[1:]
+        if val in key:
+            values.append(dict_code[val])
+
+try:
+    os.remove("result.txt")
+except:
+    pass
+
+with open("result.txt", mode='w') as temp:
+    pass
+
+with open("result.txt", encoding="utf-8", mode='w') as result:
+    for i in values:
+        result.write(i+'\n')
