2581cd3eb1454ad03756bcdf5962f9e1a31a45bb
tturlin <theo.turlin.22@gmail.com>
2018-10-20 04:05:21 +0200

    removing key from afnor file

---
+++ recuperation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author : Nicolas
+"""
+cle=["ud","sh","gc","ge","rd","rf","rj","rl","rm","rn","rp","pj","pm","qd","qh"]
+with open("181017151004_2119332-Anonyme.AFN.txt", 'r', encoding='utf-8') as file:
+    for line in file:
+        if str(line[0]+line[1]) in cle:
+            with open("AFN_transpose", 'a') as result:
+                result.write(line[3:])
