097e45de3b459e1d7ae2d8b0b07e7430d4c608f2
Nico Bellemont <nico.bellemont@gmail.com>
2018-10-21 01:52:21 +0200

    adding the warning

--- functions.py
+++ functions.py
@@ -8,10 +8,10 @@ import poche
 import prescription
 
 DICT_CODE = dict()
+
 KEY = []
 KEYS = []
 VALUES = []
-
 
 
 
@@ -169,39 +169,44 @@ def compare_prescription(presc1, presc2):
                        "Date_naissance":presc2.get_naissance,
                        "Lieu_naissance":presc2.get_lieu_naissance
                       }
+    fatal_error = ["Name", "Etablissement", "Bon_commande", "Id_etablissement",\
+                   "Contenu", "Code_ID"]
 
     for i in dict_attribute1:
         if dict_attribute1[i]() != dict_attribute2[i]():
-            print("Error at {0}, {1} does not match with {2}".format(i, dict_attribute1[i](),\
-            dict_attribute2[i]()))
-        else:
-            error = False
-
-    if not error:
-        print("Identity match 100%. Starting comparing blooc pocket.")
+            if i in fatal_error:
+                print("Fatal Error!! at {0}, {1} does not match with {2}".format(i,\
+                dict_attribute1[i](), dict_attribute2[i]()))
+                return
+            else:
+                print("Important Warning! Take a look at {0}, {1} does not \
+match with {2}".format(i, dict_attribute1[i](), dict_attribute2[i]()))
+    print("Identity match 100%. Starting comparing blood pocket.")
 
     poche_presc1 = presc1.get_poches()
     poche_presc2 = presc2.get_poches()
-    l = 0
+    L = 0
 
     for i, j in zip(poche_presc1.values(), poche_presc2.values()):
-        l += 1
-        error = False
-        dict_i={"Contenu":i.get_contenu,
-                "Phenotype":i.get_phenotype,
-                "Irradier":i.get_irradier,
-                "Code_ID":i.get_code_identification
-               }
-        dict_j={"Contenu":j.get_contenu,
-                "Phenotype":j.get_phenotype,
-                "Irradier":j.get_irradier,
-                "Code_ID":j.get_code_identification
-               }
+        L += 1
+        dict_i = {"Contenu":i.get_contenu,
+                  "Phenotype":i.get_phenotype,
+                  "Irradier":i.get_irradier,
+                  "Code_ID":i.get_code_identification
+                 }
+        dict_j = {"Contenu":j.get_contenu,
+                  "Phenotype":j.get_phenotype,
+                  "Irradier":j.get_irradier,
+                  "Code_ID":j.get_code_identification
+                 }
 
         for k in dict_i:
             if dict_i[k]() != dict_j[k]():
-                print("Error at {0}, {1} does not match with {2} for blood pockect {3}".format(k,\
-                dict_i[k](), dict_j[k](), l))
-                error = True
-    if not error :
-        print("Identity match 100%. compareason is over.")
+                if k in fatal_error:
+                    print("Fatal Error!! at {0}, {1} does not match with {2}".format(k,\
+                    dict_i[k](), dict_j[k]()))
+                    return
+                else:
+                    print("Important Warning! Take a look at {0}, {1} does not \
+match with {2}".format(i, dict_i[k](), dict_j[k]()))
+    print("Identity match 100%. compareason is over.")
