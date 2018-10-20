0cb53732f9fff1a19ad6dc01c72e6d76020cb6a3
Nico Bellemont <nico.bellemont@gmail.com>
2018-10-20 20:54:36 +0200

    change the elif for a dict

--- functions.py
+++ functions.py
@@ -7,6 +7,10 @@ import pickle
 import poche
 import prescription
 
+
+
+
+
 def recuperation(inputname, outputname):
     sh = 0
     ud = 0
@@ -20,7 +24,7 @@ def recuperation(inputname, outputname):
     with open(outputname, mode='w'):
         pass
 
-    cle = ["ud", "sh", "gc", "ge", "rd", "rf", "rj", "rl", "rm", "rn", "rp", "pj", "pm", "qd", "ej"]
+    cle = ["ud", "sh", "ge", "rd", "rf", "rj", "rl", "rm", "rn", "rp", "pj", "pm", "qd", "ej"]
     with open(inputname, 'r', encoding='utf-8') as file:
         for line in file:
             if line[0]+line[1] in cle:
@@ -101,40 +105,32 @@ def analysis(inputname, outputname):
     with open(inputname) as temp:
         spamreader = csv.reader(temp)
         for row in spamreader:
-            if row[0] == 'pj':
+            if row[0] == "pj":
                 i += 1
-                pocket["poche_"+str(i)] = poche.Poche(row[1])
-            elif row[0] == 'pm':
-                pocket["poche_"+str(i)].set_code_identification(row[1])
-            elif row[0] == 'qd':
-                if 'Irradié' in row[1]:
-                    pocket["poche_"+str(i)].set_irradier(True)
+                pocket["poche_" + str(i)] = poche.Poche(row[1])
+            elif row[0] == "pm":
+                pocket["poche_" + str(i)].set_code_identification(row[1])
+            elif row[0] == "qd":
+                if "Irradié" in row[1]:
+                    pocket["poche_" + str(i)].set_irradier(True)
                 else:
-                    pocket["poche_"+str(i)].set_phenotype(True)
+                    pocket["poche_" + str(i)].set_phenotype(True)
             elif row[0] == "ej":
                 file = prescription.Prescription(row[1])
-            elif row[0] == "ud":
-                file.set_service_prescripteur(row[1])
-            elif row[0] == "sh":
-                file.set_etablissement(row[1])
-            elif row[0] == "ge":
-                file.set_bon_commande(row[1])
-            elif row[0] == "rd":
-                file.set_nomf(row[1])
-            elif row[0] == "rf":
-                file.set_pnom(row[1])
-            elif row[0] == "rj":
-                file.set_sexe(row[1])
-            elif row[0] == "rl":
-                file.set_naissance(row[1])
-            elif row[0] == "rm":
-                file.set_lieu_naissance(row[1])
-            elif row[0] == "rn":
-                file.set_id_etablissement(row[1])
-            elif row[0] == "rp":
-                file.set_id_inlog(row[1])
+                dico = {"ud" : file.set_service_prescripteur,
+                        "sh" : file.set_etablissement,
+                        "ge" : file.set_bon_commande,
+                        "rd" : file.set_nomf,
+                        "rf" : file.set_pnom,
+                        "rj" : file.set_sexe,
+                        "rl" : file.set_naissance,
+                        "rm" : file.set_lieu_naissance,
+                        "rn" : file.set_id_etablissement,
+                        "rp" : file.set_id_inlog
+                       }
             else:
-                pass
+                dico[row[0]](row[1])
+
 
         file.set_poches(pocket)
 
