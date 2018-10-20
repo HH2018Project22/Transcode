7512c8b35653da0d37f014a2ebfba750ffc734d8
tturlin <theo.turlin.22@gmail.com>
2018-10-20 16:46:16 +0200

    modify the ej correspondance

--- recuperation.py
+++ recuperation.py
@@ -15,6 +15,7 @@ with open("AFN_transpose.csv", mode='w') as temp:
 
 sh = 0
 ud = 0
+ej = 0
 cle=["ud","sh","gc","ge","rd","rf","rj","rl","rm","rn","rp","pj","pm","qd","ej"]
 with open("181017151004_2119332-Anonyme.AFN.txt", 'r', encoding='utf-8') as file:
     for line in file:
@@ -27,6 +28,11 @@ with open("181017151004_2119332-Anonyme.AFN.txt", 'r', encoding='utf-8') as file
             elif line[0]+line[1] == "ud":
                 ud += 1
                 if ud == 1:
+                    with open("AFN_transpose.csv", 'a') as result:
+                        result.write(line)
+            elif line[0]+line[1] == "ej":
+                ej += 1
+                if ej == 1:
                     with open("AFN_transpose.csv", 'a') as result:
                         result.write(line)
             else:
