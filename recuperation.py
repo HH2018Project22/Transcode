45d3fc6416c42a3703bc01d4c0515e29a520ab94
Nico Bellemont <nico.bellemont@gmail.com>
2018-10-20 14:34:47 +0200

    remove qh key

--- recuperation.py
+++ recuperation.py
@@ -15,7 +15,7 @@ with open("AFN_transpose.csv", mode='w') as temp:
 
 sh = 0
 ud = 0
-cle=["ud","sh","gc","ge","rd","rf","rj","rl","rm","rn","rp","pj","pm","qd","qh"]
+cle=["ud","sh","gc","ge","rd","rf","rj","rl","rm","rn","rp","pj","pm","qd"]
 with open("181017151004_2119332-Anonyme.AFN.txt", 'r', encoding='utf-8') as file:
     for line in file:
         if line[0]+line[1] in cle:
